81a10f32329ababffd198c6c4d1f90f93211ad23
Sreehari <sreehari14shr@gmail.com>
2024-09-24 18:19:20 +0530

    queue using linked list

---
+++ queue_linkedlist.c
@@ -0,0 +1,84 @@
+//PROGRAM ON LINKED LIST IMPLEMENTATION on QUEUE
+// BY SREEHARI
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+typedef struct node
+{
+	int data;
+	struct node * next;
+}node;
+node *head = NULL;
+void InsertionEnd(){
+	int item;
+	node *new;
+	node *temp;
+	printf("Enter the data to be added: ");
+	scanf("%d", &item);
+	new = (node *)malloc(sizeof (node));
+	new->data = item;
+	new->next = NULL;
+	if(head == NULL){
+		head = new;
+	}
+	else{
+		temp = head;
+		while(temp->next != NULL){
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	
+}
+void deleteBeginning(){
+	if(head==NULL){
+		printf("Linked List is Empty");
+	}
+	else{
+		head = head->next;
+	}
+}
+void display(){
+	node * temp;
+	if(head==NULL){
+		printf("Linked List is Empty");
+	}
+	else{
+		temp = head;
+		while(temp->next != NULL){
+			printf("%d \t", temp->data);
+			temp = temp->next;
+		}
+		printf("%d", temp->data);
+
+	}
+}
+
+void main(){
+	while(1){
+	int n;
+	printf("\n 1. Insertion\ 
+		\n 2. Delete \
+		\n 3. Display \
+		\n 4. Exit");
+	printf("\nEnter your choice:");
+	scanf("%d", &n);
+	switch(n){
+		case 1:
+			InsertionEnd();
+			break;
+		case 2:
+			deleteBeginning();
+			break;
+        case 3:
+			display();
+			break;
+		case 4:
+			exit(0);
+			break;
+		default:
+			printf("Enter a valid choice");
+	
+		}
+	 }
+}
