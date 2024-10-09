589dac0a51d2e0c4bea14abae3f2a25cde1b9a65
Sreehari <142385007+Sree14hari@users.noreply.github.com>
2024-10-09 18:29:49 +0530

    tree traversals

---
+++ preorder.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node *lchild;
+struct node *rchild;
+};
+
+struct node *newnode(int item){
+struct node *new=(struct node*)malloc(sizeof(struct node));
+new->data=item;
+new->lchild=NULL;
+new->rchild=NULL;
+return new;
+}
+void preorder(struct node *root){
+if (root==NULL){
+return;
+}
+printf("\t%d",root->data);
+preorder(root->lchild);
+preorder(root->rchild);
+}
+void main(){
+struct node *root=NULL;
+root=newnode(1);
+root->lchild=newnode(2);
+root->rchild=newnode(3);
+root->lchild->lchild=newnode(4);
+root->lchild->rchild=newnode(5);
+root->rchild->lchild=newnode(6);
+root->rchild->rchild=newnode(7);
+preorder(root);
+}
